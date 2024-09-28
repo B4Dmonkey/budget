bceeb5efefed2435d80c5561ad803d4b574021ca
josiah <josiah@appstack.nyc>
2024-09-27 21:35:56 -0400

    upload documents

--- handlers.go
+++ handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +25,25 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	// * save the file to disk
+	// todo: check if the folder
+	out, err := os.Create("uploads/" + header.Filename)
+	if err != nil {
+		log.Println("Error creating file:", err)
+		http.Error(w, "Unable to create the file", http.StatusInternalServerError)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Println("Error saving file:", err)
+		http.Error(w, "Unable to save the file", http.StatusInternalServerError)
+		return
+	}
+	// * save the location to the database
+	// * go through the db and create or update the records
+	// * respond with the values
 	// Log the file name
 	log.Printf("Uploaded file: %s", header.Filename)
 
